a584a71625e3b8729146994f6540a05030670c8e
Laishuxin <56504759+Laishuxin@users.noreply.github.com>
2021-09-14 23:28:09 +0800

    feat: 基础样式

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className='App'>
+      <h1>app</h1>
+      <a href='#' title='link'>
+        dummy link
+      </a>
+      <hr />
+      <code lang='javascript'>const a = "1" let b = "2"</code>
+    </div>
+  )
+}
+
+export default App
